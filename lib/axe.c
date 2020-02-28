8d2d5c087ecb90ba2a96c66e7dc930dcfbe39c01
poilynx <lihsilyn@gmail.com>
2020-02-29 07:47:04 +0800

    Add ax_init and ax_uninit func

--- lib/axe.c
+++ lib/axe.c
@@ -1,8 +1,50 @@
 #include "axe.h"
 #include "debug.h"
 #include <stdio.h>
-void ax__init(int trace)
+#include <stdlib.h>
+
+static int init_count = 0;
+
+void ax__init(ax_bool_t debug, ax_bool_t trace)
 {
-	ax_debug_trace = trace;
+	if (init_count > 0) {
+		if(init_count > 0xFFFF) {
+			ax_perror("Too many times to initialize");
+		} else
+			init_count ++;
+		return;
+	}
+#ifdef AX_DEBUG
+	ax_bool_t lib_debug = ax_true;
+#else
+	ax_bool_t lib_debug = ax_false;
+#endif
+	if (lib_debug != debug) {
+		if (debug)
+			fputs("error:ax_init:program defined macro AX_DEBUG, but axe library did not\n", stderr);
+		else
+			fputs("error:ax_init:program did not define macro AX_DEBUG, but axe library did\n", stderr);
+		exit(EXIT_FAILURE);
+	}
 	ax_debug_set_fd(stdout);
+	ax_debug_trace = trace;
+	init_count++;
+}
+
+void ax__uninit()
+{
+	if(init_count > 1) {
+		
+		init_count--;
+		return;
+	} else if (init_count == 0)
+		return;
+	ax_debug_set_fd(NULL);
+	ax_debug_trace = ax_false;
+	init_count--;
+}
+
+int ax__init_count()
+{
+	return init_count;
 }
--- lib/axe.h
+++ lib/axe.h
@@ -1,11 +1,24 @@
 #ifndef AXE_H_
 #define AXE_H_
-
-void ax__init(int trace);
+#include "def.h"
+void ax__init(ax_bool_t debug, ax_bool_t trace);
+void ax__uninit();
 #ifdef AX_TRACE
-# define ax_init() ax__init(1)
+# define __TRACE_ENABLE 1
 #else
-# define ax_init() ax__init(0)
+# define __TRACE_ENABLE 0
 #endif
+
+#ifdef AX_DEBUG
+# define __DEBUG_ENABLE 1
+#else
+# define __DEBUG_ENABLE 0
+#endif
+
+#define ax_init() ax__init(__DEBUG_ENABLE, __TRACE_ENABLE)
+#define ax_uninit() ax__uninit()
+
+#undef __TRACE_ENABLE
+#undef __DEBUG_ENABLE
 
 #endif
