24861ee3b3677c2a1eaf3b54dcf3420e0a44451f
poilynx <lihsilyn@gmail.com>
2020-02-29 11:19:58 +0800

    Add ax_foreach macro for iterating box conveniently

--- lib/axe.h
+++ lib/axe.h
@@ -1,24 +1,33 @@
 #ifndef AXE_H_
 #define AXE_H_
 #include "def.h"
+#include "iter.h"
+#include "box.h"
 void ax__init(ax_bool_t debug, ax_bool_t trace);
 void ax__uninit();
+
 #ifdef AX_TRACE
-# define __TRACE_ENABLE 1
+# define AX__TRACE_ENABLE 1
 #else
-# define __TRACE_ENABLE 0
+# define AX__TRACE_ENABLE 0
 #endif
 
 #ifdef AX_DEBUG
-# define __DEBUG_ENABLE 1
+# define AX__DEBUG_ENABLE 1
 #else
-# define __DEBUG_ENABLE 0
+# define AX__DEBUG_ENABLE 0
 #endif
 
-#define ax_init() ax__init(__DEBUG_ENABLE, __TRACE_ENABLE)
+#define ax_init() ax__init(AX__DEBUG_ENABLE, AX__TRACE_ENABLE)
 #define ax_uninit() ax__uninit()
 
-#undef __TRACE_ENABLE
-#undef __DEBUG_ENABLE
+#define ax_foreach(_r, _a) \
+	for ( char __for_flag = 1 ; __for_flag ; ) \
+	for ( ax_ref_t _r ; __for_flag ; __for_flag = 0) \
+	for ( \
+		ax_iter_t _it = ax_box_begin((_a)), __##_it##_end = ax_box_end((_a)); \
+		!ax_iter_equal(&(_it), &(__##_it##_end)) && ((_r) = ax_iter_get(&_it, 0), 1); \
+		ax_iter_shift(&_it, 1) \
+	)
 
 #endif
