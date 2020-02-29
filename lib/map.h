bd73c007ee3db88c4262f7cc0d0ce6bf9776283d
poilynx <lihsilyn@gmail.com>
2020-02-29 11:17:40 +0800

    Add map traits

--- lib/map.h
+++ lib/map.h
@@ -23,4 +23,49 @@
 #ifndef MAP_H_
 #define MAP_H_
 
+#include "any.h"
+#include "box.h"
+#include "debug.h"
+#include "pair.h"
+struct ax_stuff_trait_st;
+typedef struct ax_stuff_trait_st ax_stuff_trait_t;
+
+struct ax_map_st;
+typedef struct ax_map_st ax_map_t;
+
+typedef int      (*ax_map_put_f)(ax_any_t* any, ax_ref_t key, ax_ref_t value);
+typedef ax_iter_t(*ax_map_get_f)(ax_any_t* any, ax_ref_t key);
+typedef ax_bool_t(*ax_map_exist_f)(ax_any_t* any, ax_ref_t key);
+typedef ax_bool_t(*ax_map_erase_f)(ax_any_t* any);
+typedef ax_iter_t(*ax_map_find_f)(ax_any_t* any, ax_ref_t value);
+
+struct ax_map_trait_st
+{
+	ax_map_put_f put;
+	ax_map_get_f get;
+	ax_map_exist_f exist;
+	ax_map_find_f find;
+};
+typedef struct ax_map_trait_st ax_map_trait_t;
+
+struct ax_map_st
+{
+	ax_box_t box;
+	const ax_map_trait_t tr;
+	const ax_pair_trait_t pair_tr;
+	const ax_stuff_trait_t* elem_tr;
+};
+
+int       ax_map_put  (ax_any_t* any, ax_ref_t key, ax_ref_t value);
+ax_iter_t ax_map_get  (ax_any_t* any, ax_ref_t key);
+ax_iter_t ax_map_find (ax_any_t* any, ax_ref_t value);
+ax_bool_t ax_map_exist(ax_any_t* any, ax_ref_t key);
+ax_bool_t ax_map_erase(ax_any_t* any);
+
+#define ax_map_put  (_a, _k, _v) (ax_step(ax_map_put  ), ax_map_put  (_a, _k, _v))
+#define ax_map_get  (_a, _k)     (ax_step(ax_map_get  ), ax_map_get  (_a, _k))
+#define ax_map_find (_a, _v)     (ax_step(ax_map_find ), ax_map_find (_a, _v))   
+#define ax_map_exist(_a, _k)     (ax_step(ax_map_exist), ax_map_exist(_a, _k))  
+#define ax_map_erase(_a)         (ax_step(ax_map_erase), ax_map_erase(_a))
+
 #endif
